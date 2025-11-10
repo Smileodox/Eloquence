5f3b0409859f7537417367d476da97a093af77af
Johannes Gruber <johannes-carl.gruber@nordakademie.de>
2025-11-10 17:40:12 +0100

    Initial Commit

---
+++ Eloquence/EloquenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EloquenceApp.swift
+//  Eloquence
+//
+//  Created by Johannes Gruber on 10.11.25.
+//
+
+import SwiftUI
+
+@main
+struct EloquenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
